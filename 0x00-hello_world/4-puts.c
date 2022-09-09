731fdc3b9ace260ddf5cd0381b2ec84f6b900fef
jamesamad88 <jamesamadi88@gmail.com>
2022-09-08 20:05:38 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/*
+ * Entry point
+ * main-prints the phrase programming is like building a multiliqual
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilinqual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+/* Entry point */
+int main(void)
+{
+	printf("with proper grammer but the outcome is a piece of art,\n");
+	return (0);
+}
