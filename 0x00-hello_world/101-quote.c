e6bb4a66495e68acf4f4d5d609343f7d51b62b84
jamesamad88 <jamesamadi88@gmail.com>
2022-09-08 20:39:59 -0700

    New

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <unistd.h>
+/**
+ * main - first line
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\"-Dora Korpa,2005-10-19\n", 59);
+	return (1);
+}
