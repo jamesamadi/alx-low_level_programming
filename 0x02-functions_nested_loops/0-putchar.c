10a8025a70c01fdbf1346af80bd01c7d842db8f8
jamesamad88 <jamesamadi88@gmail.com>
2022-09-13 14:33:18 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - to display something
+ *
+ * return: 0 if true
+ */
+int main (void)
+{
+	_putchar("%d\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef main
+#define ma:u
+int _putchar(char c);
+
+#endif
