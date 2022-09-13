fae5f0d0eae147f37626767f3183b5116a21008f
jamesamad88 <jamesamadi88@gmail.com>
2022-09-13 16:11:21 -0700

    n

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes some _putchar
+ *
+ * Decription:  prints _printchar
+ *
+ * Return: 0 (Always)
+ */
+
+void print_alphabet(void)
+{
+char a;
+	for (a < z)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n')
+}
+
