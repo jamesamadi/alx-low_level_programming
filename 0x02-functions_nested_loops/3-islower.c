27c9e20713c24ac506a246f4cbd22c20ccd58464
jamesamad88 <jamesamadi88@gmail.com>
2022-09-14 08:26:45 -0700

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - prints alphabet 10 times
+ *
+ * Description: Make a while loop'
+ *
+ * Return: void
+ */
+
+int _islower(void);
+{
+	int c;
+
+	for (c >= 'a'; c <= 'z';)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
