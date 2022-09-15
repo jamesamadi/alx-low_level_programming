162e7d8de44cf4477cebf055629ee354a67ab68b
jamesamad88 <jamesamadi88@gmail.com>
2022-09-15 16:19:58 -0700

    n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints alphabet 10 times
+ * print_times_table - prints alphabet 10 times
  *
  * Description: Make a while loop'
  * @n: integer numbers
@@ -29,6 +29,13 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar('0' + m);
 				}
+				else if (m < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + p);
+					_putchar('0' + m % 10);
+				}
 				else
 				{
 					_putchar(' ');
@@ -36,10 +43,10 @@ void print_times_table(int n)
 					_putchar('0' + p % 10);
 					_putchar('0' + m % 10);
 				}
-				if (y < n)
-					_putchar(',');
-				else
-					_putchar('\n');
+			if (y < n)
+				_putchar(',');
+			else
+				_putchar('\n');
 			}
 		}
 	}
