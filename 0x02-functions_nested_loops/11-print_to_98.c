0bb1e583f6671f5e6054245f9572c6e845d31806
jamesamad88 <jamesamadi88@gmail.com>
2022-09-15 15:35:28 -0700

    n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 			if (i != 98)
 			{
 				_putchar(',');
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 			if (i != 98)
 			{
 				_putchar(',');
