0776951acd28e81991fda6ca15b213d41df8a8ec
jamesamad88 <jamesamadi88@gmail.com>
2022-09-16 14:44:51 -0700

    n

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - return a valur for upper case
+ * @c: defines c as some integer
+ * Return: returns 1 for success
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	if (i >= 'A' && i <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
