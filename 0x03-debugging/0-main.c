fc3e238bcb18b4a2ba256395dcb452a6e9390629
jamesamad88 <jamesamadi88@gmail.com>
2022-09-16 13:23:37 -0700

    n

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- *
  * main - test some files
  *
  * Return: void
  */
- 
- int main (void)
+
+int main(void)
 {
 int i;
 
- i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
 return (0);
-
 }
