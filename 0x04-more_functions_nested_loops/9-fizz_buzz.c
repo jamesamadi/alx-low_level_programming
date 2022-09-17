52be2f6ac4ffb823dbb0c528d77d2f7a4aca3964
jamesamad88 <jamesamadi88@gmail.com>
2022-09-17 10:49:59 -0700

    n

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return a valur for upper case
+ * Description: defines c as some integer
+ * Return: returns 1 for success
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n < 100 || n == 100)
+	{
+
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
