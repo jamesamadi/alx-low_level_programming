6a77851f4607306167d430a375654a7352870c77
jamesamad88 <jamesamadi88@gmail.com>
2022-09-16 17:01:14 -0700

    n

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - return a valur for upper case
+ * Description: prints
+ *
+ * Return: returns 1 for success
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
