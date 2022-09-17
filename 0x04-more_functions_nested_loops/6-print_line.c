1e298ed27236fb18c22b5801fe85d16ad5f39e1f
jamesamad88 <jamesamadi88@gmail.com>
2022-09-16 19:53:12 -0700

    n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,16 @@ void print_line(int n)
 {
 	int a = 0;
 
-	if (n > 0)
+	if (n > 0;)
 	{
 		for  (a < n)
 		{
-			_putcha('_');
+			_putchar('_');
 			a++;
 		}
 		_putchar('_');
 	}
 	else
 		_putchar('\n');
-	return (0);
+	return (void);
 }
