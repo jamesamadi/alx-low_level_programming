13bdb7eb959cf716b830dbc4d3052ccf9c605484
jamesamad88 <jamesamadi88@gmail.com>
2022-09-17 11:35:27 -0700

    n

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - return a valur for upper case
+ * @size: defines c as some integer
+ *
+ * Return: returns 1 for success
+ */
+
+void print_triangle(int size)
+{
+	int x = 1, y;
+
+	while (x <= size && size > 0)
+	{
+		y = 0;
+
+		while (y < size - 1)
+		{
+			_putchar(' ');
+			y++;
+		}
+		y = 0;
+
+		while (y < x)
+		{
+			_putchar('#')
+		}
+		_putchar('\n');
+		x++;
+	}
+	if (x == 1)
+		_putchar('\n');
+}
