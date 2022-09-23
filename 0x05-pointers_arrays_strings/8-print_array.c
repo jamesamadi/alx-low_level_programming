ff3e1d5a40af4eba144ee0623981b7472a58b9f3
jamesamad88 <jamesamadi88@gmail.com>
2022-09-23 07:49:22 -0700

    n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * prints_array - to point a pointer to 98
+ * @n: interger value
+ * @a: integer value
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	_putchar('\n');
+}
