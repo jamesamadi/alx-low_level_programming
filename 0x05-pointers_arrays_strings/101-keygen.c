badf7eadfda607ac4dd94dff689cef5b50abea32
jamesamad88 <jamesamadi88@gmail.com>
2022-09-23 10:18:02 -0700

    n

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - to point a pointer to 98
+ * Description: interger value
+ * Return: void
+ */
+
+int main(void)
+{
+	int d, a = 0;
+	int b = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (b < 1000)
+	{
+		a = rand() % 28;
+		if ((b + a) > 1000)
+			break;
+		b = b + a;
+		printf("%c", a);
+	}
+	d = 2772 - b;
+	printf("%c\n", b);
+	return (0);
+}
