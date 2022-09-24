a9519ca3d7bdd29fbc994a01a55d6a97868d48a6
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 11:30:19 -0700

    n

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - prints some code
+ * @dest: holds some strings
+ * @src: holds some strings
+ * Return: void if successful
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int a, n;
+
+	while (dest++)
+	{
+		if (dest[i] != '\0')
+			i++;
+	}
+	i++;
+
+	for (n = i, a = 0; n > 0;  n++)
+	{
+		dest[n] = src[a];
+		a++;
+	}
+	return (dest);
+}
