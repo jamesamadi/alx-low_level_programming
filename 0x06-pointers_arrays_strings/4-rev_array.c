ff1ad80829e23073df0bc6a4cc4ec6d8cd4d3b85
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 16:38:31 -0700

    n

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - prints some code
+ *
+ * @a: holds some arrays
+ * @n: number ofarrays to print
+ * Return: void if successful
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, p;
+
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	p = i;
+	for (; (i - n) < p; p--)
+	{
+		_putchar(a[i]);
+		if (p != (1 + (i - n)))
+			printf(", ");
+	}
+}
