343758d228f2678dd4001638de36aa4854d2c86f
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 13:48:18 -0700

    n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 	}
 	for  (; i < n; i++)
 	{
