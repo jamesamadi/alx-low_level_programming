1aa7aad488b88d77d70d6988a377c1fa863399d6
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 12:17:01 -0700

    n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - prints some code
+ * @dest: holds some strings
+ * @src: holds some strings
+ * @n: total number to hold
+ * Return: void if successful
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int a;
+
+	while (src[i] != '\0' && i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
