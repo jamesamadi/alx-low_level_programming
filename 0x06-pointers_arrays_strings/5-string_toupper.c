3802f6a475858b2b996f070dde292e980911ab7a
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 20:36:50 -0700

    n

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _string_upper - prints some code
+ * @s: holds some strings
+ *
+ *
+ * Return: void if successful
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
