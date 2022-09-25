0a31a0a732adeaa5e41ce6904a84a80f3fd46ed4
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 20:48:13 -0700

    n

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _string_upper - prints some code
+ * string_toupper - prints some code
  * @s: holds some strings
  *
  *
@@ -10,15 +10,13 @@
 
 char *string_toupper(char *s)
 {
-	int j;
 	int i = 0;
 
-	for (j = 0; s[j] != '\0'; j++)
+	for  (; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
-			i++;
 		}
 	}
 	return (s);
