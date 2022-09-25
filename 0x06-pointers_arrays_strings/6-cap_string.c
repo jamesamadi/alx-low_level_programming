af7f3c25a5736db17045d365697223131f1e25cf
jamesamad88 <jamesamadi88@gmail.com>
2022-09-24 21:37:46 -0700

    n

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - prints some code
+ * @s: holds some strings
+ *
+ *
+ * Return: void if successful
+ */
+
+char *cap_string(char *s)
+{
+	int j = 0;
+	char c = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, ' ', '\t', '\n'};
+	int i = 0;
+
+	while (s[j])
+	{
+		for (i = 0; i < 13; i++)
+			if (s[j] >= 'a' && s[j] <= 'z' && s[j - 1] == c[i])
+			{
+				s[j] = s[j] - 32;
+			}
+		j++;
+	}
+	return (s);
+}
