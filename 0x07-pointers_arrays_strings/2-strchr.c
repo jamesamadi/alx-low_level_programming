2263bc8f4317241619fc28eb27a1d6831feec433
jamesamad88 <jamesamadi88@gmail.com>
2022-09-26 18:45:21 -0700

    n

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,7 +11,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	int n = 0;
+	int j, n = 0;
 	char *p;
 
 	for (; s[i] != '\0'; i++)
@@ -20,6 +20,8 @@ char *_strchr(char *s, char c)
 		{
 			j = i + n;
 			p[n]  = s[j];
+			n++;
+
 			return (p);
 		}
 		else
