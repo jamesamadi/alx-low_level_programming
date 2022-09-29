20f8202b7c24e80fd1513416e517a45137baa901
jamesamad88 <jamesamadi88@gmail.com>
2022-09-29 07:33:50 -0700

    n

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints some code
+ * @n: holds some strings
+ * holds some strings
+ * number of characters
+ * Return: void if successful
+ */
+
+void print_number(int n)
+{
+	int i, j, k;
+
+	k = 10;
+	if (n == '-')
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	for (j = n % k, i = 0; n > 0; i++)
+	{
+		_putchar('j');
+		n = j;
+		k = k * 10;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+}
