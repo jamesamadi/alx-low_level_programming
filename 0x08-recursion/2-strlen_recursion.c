25850f9c615e6d2f8752e9048b6dc8077982aadb
jamesamad88 <jamesamadi88@gmail.com>
2022-09-29 16:52:12 -0700

    n

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - used to puts characters to a screen
+ * @s: string to be displayed
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (0);
+	else 
+		return 1 + _strlen_recursion(s + 1);
+}
