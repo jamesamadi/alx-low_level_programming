8981adf61269e828c267308374695eb82c481a6a
jamesamad88 <jamesamadi88@gmail.com>
2022-09-29 16:12:49 -0700

    n

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,16 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	int n = 0;
-
-	if (s[n] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 
 	}
 
-	_putchar(s[n]);
-	n++;
-	_puts_recursion(s[n]);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
