4369705f17a8e2d129505312a256e24ca16e8ff0
jamesamad88 <jamesamadi88@gmail.com>
2022-09-29 20:03:59 -0700

    n

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * find_sqrt - used to puts characters to a screen
+ * @num: some integer to be displayed
+ * @root: some integer
+ * Return: void
+ */
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root - 1));
+}
+
+/**
+ * _sqrt_recursion - used to find square root of numbers
+ * @n: interger to find its square root
+ *
+ * Return: void
+ */
+
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
+}
